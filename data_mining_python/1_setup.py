02df8b5de45945c00420d833f1f7e0381b5810d3
junholeephy <skyblue1293@naver.com>
2018-02-04 00:24:09 +0100

    Update 1_setup.py

--- data_mining_python/1_setup.py
+++ data_mining_python/1_setup.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()   # if the path of the chromedriver is not set at your local $PATH, one need th fill the path
+driver = webdriver.Chrome()   # if the path of the chromedriver is not set at your local $PATH, one need to fill the path
 driver.get("https://nid.naver.com/nidlogin.login")
 
 
