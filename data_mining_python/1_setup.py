0362210340f29defb947bdaf981fd6b09c645a60
junholeephy <skyblue1293@naver.com>
2018-02-03 21:55:06 +0100

    python web crawling

---
+++ data_mining_python/1_setup.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()   # if the path of the chromedriver is not set at your local $PATH, one need th fill the path
+driver.get("https://nid.naver.com/nidlogin.login")
+
+
+
+
