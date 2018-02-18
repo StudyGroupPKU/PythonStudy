f1b4164217d7d505e78fd11b09f3f6cb22037a54
junholeephy <skyblue1293@naver.com>
2018-02-18 20:29:36 +0100

    deep_learning_from_Scratch

---
+++ DL_Ko/deep-learning-from-scratch/common/gradient.py
@@ -0,0 +1,63 @@
+# coding: utf-8
+import numpy as np
+
+def _numerical_gradient_1d(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # 값 복원
+        
+    return grad
+
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
+        
+        return grad
+
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+#    print("f output", f(0.456))
+#    print("f output", f(0.4561))
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+#        print("x1 output", x[idx])
+#        print("f1 output1", f(x[idx]))
+#        print("f1 output2", f(x[idx]-0.0002))
+
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+
+#        print("x2 output", x[idx])
+#        print("f2 output", f(x[idx]))
+       # print("f1",fxh1)
+       # print("f2",fxh2)
+       # print("f1-f2",fxh1-fxh2)
+
+        x[idx] = tmp_val # 값 복원
+        it.iternext()   
+        
+    return grad
